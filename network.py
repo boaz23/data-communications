96c03fa8cf592984504597bf3353d2bf11ece663
Boaz Rozovski <boazrozo@post.bgu.ac.il>
2020-12-25 10:40:58 +0200

    Circular dependancy

--- network.py
+++ network.py
@@ -1,16 +1,20 @@
-import config
 from scapy.all import *
+
+ADDR_LOCALHOST = ''
+
+class NetIf:
+    def __init__(self, name, broadcast_address):
+        self.name = name
+        self.broadcast_address = broadcast_address
+
+    def addr(self):
+        return get_if_addr(self.name)
+
+#--------------------
+import config
 
 def my_addr():
     return config.active_network_interface.addr()
 
 def broadcast_addr():
     return config.active_network_interface.broadcast_address
-
-class NetIf:
-    def __init__(self, name, broadcast_address):
-        self.if_name = name
-        self.broadcast_address = broadcast_address
-
-    def addr(self):
-        return get_if_addr(config.name)
