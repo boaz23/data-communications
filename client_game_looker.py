b6d2e90c5991f6e586c1b710db0e668b5bf8e0ae
Boaz Rozo <boazrozo@post.bgu.ac.il>
2020-12-26 20:14:27 +0200

    Client documentation
    Documneted the client game lookup module

--- client_game_looker.py
+++ client_game_looker.py
@@ -1,3 +1,13 @@
+"""Client logic for game lookup
+
+Contains the logic of the client needed to look for game offers.
+
+Functions:
+    * look_for_game():
+        Looks for a game offers from a server and returns its
+        address and port
+"""
+
 import socket
 
 import config
@@ -6,9 +16,16 @@ import coder
 import util
 from socket_address import SocketAddress
 
+# This is the address which we will listen for packets
 _game_offer_recv_addr = SocketAddress((network.my_addr(), config.GAME_OFFER_PORT))
 
 def look_for_game():
+    """Looks for a game offer and returns the server with the port
+
+    Initiates a UDP socket and listens for a game offer and
+    returns the server with the port.
+    """
+
     print(f"waiting for game offer, listening on {_game_offer_recv_addr}")
     try:
         game_offer_socket = _init_game_offer_socket()
@@ -20,12 +37,23 @@ def look_for_game():
     return server_addr
 
 def _init_game_offer_socket():
+    """Initiates the socket
+
+    Initiates the UDP socket used to listen for packets.
+    """
     game_offer_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     game_offer_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     game_offer_socket.bind(_game_offer_recv_addr.to_tuple())
     return game_offer_socket
 
 def _recv_game_offer(game_offer_socket):
+    """Receive game offer and return it
+
+    Blocks to receive UDP packets.
+    Checks if they represent a proper game offer, and if so,
+    returns the server which sent the offer toghether with the port it
+    said we should connected to (the port in the message)
+    """
     message_bytes, server_addr = game_offer_socket.recvfrom(config.GAME_OFFER_RECV_BUFFER_SIZE)
     server_addr = SocketAddress(server_addr)
     print(f"received data from {server_addr}")
