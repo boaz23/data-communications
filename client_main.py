734cca7aedf21b1bee5c8f64bfc7bc8b3e507f44
Boaz Rozo <boazrozo@post.bgu.ac.il>
2020-12-26 19:31:48 +0200

    Client changes
    * Small refactor of the client to use a global variable for the socket
    * Added a selector for the input and socket reading split
    * Added code from stackoverflow to enable reading characters from the
    terminal without waiting for newline
    * Added better resource cleanup code (try/finally)
    * Allowed address reuse on the game offer socket since it is going to
    be opened and closed in a loop of ~10 seconds time

--- client_main.py
+++ client_main.py
@@ -1,5 +1,7 @@
 import socket
 import time
+import selectors
+import termios, fcntl, sys, os
 
 import config
 import network
@@ -8,15 +10,57 @@ import util
 from socket_address import SocketAddress
 
 game_offer_recv_addr = SocketAddress((network.my_addr(), config.GAME_OFFER_PORT))
+fd_stdin = sys.stdin.fileno()
+game_socket = None
+selector = None
 
 def main():
-    print("Client started, listening for offer requests...")
-    while True:
-        game_offer_addr = look_for_game()
-        game_socket = establish_game_connection(game_offer_addr)
-        send_team_name(game_socket)
-        #game_socket.close()
+    global fd_stdin
 
+    oldterm = termios.tcgetattr(fd_stdin)
+    newattr = termios.tcgetattr(fd_stdin)
+    newattr[3] = newattr[3] & ~termios.ICANON
+    termios.tcsetattr(fd_stdin, termios.TCSANOW, newattr)
+
+    oldflags = fcntl.fcntl(fd_stdin, fcntl.F_GETFL)
+    fcntl.fcntl(fd_stdin, fcntl.F_SETFL, oldflags | os.O_NONBLOCK)
+
+    try:
+        main_logic_loop()
+    finally:
+        termios.tcsetattr(fd_stdin, termios.TCSAFLUSH, oldterm)
+        fcntl.fcntl(fd_stdin, fcntl.F_SETFL, oldflags)
+
+def main_logic_loop():
+    global selector
+
+    print("Client started, listening for offer requests...")
+    selector = None
+    try:
+        selector = selectors.DefaultSelector()
+        while True:
+            main_logic_iter()
+    finally:
+        if selector is not None:
+            selector.close()
+
+def main_logic_iter():
+    global game_socket
+    global selector
+
+    try:
+        has_socket_been_registered = False
+        game_offer_addr = look_for_game()
+        establish_game_connection(game_offer_addr)
+        send_team_name()
+
+        game_socket.setblocking(False)
+        has_socket_been_registered = True
+    finally:
+        if has_socket_been_registered:
+            selector.unregister(game_socket)
+        if game_socket is not None:
+            game_socket.close()
 
 def look_for_game():
     print(f"waiting for game offer, listening on {game_offer_recv_addr}")
@@ -33,14 +77,15 @@ def look_for_game():
     return game_offer
 
 def establish_game_connection(game_offer_addr):
+    global game_socket
+    global selector
+
     print(f"Received offer from {game_offer_addr.host}, attempting to connect...")
     print(f"got offer from {game_offer_addr}")
     game_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    game_socket.setblocking(False)
     game_socket.connect(game_offer_addr.to_tuple())
-    return game_socket
 
-def send_team_name(game_socket):
+def send_team_name():
     game_socket.send(coder.encode_string(f"{config.TEAM_NAME}\n"))
 
 def init_game_offer_socket():
@@ -53,6 +98,7 @@ def create_game_offer_socket():
     while game_offer_socket is None:
         try:
             game_offer_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            game_offer_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         except OSError as e:
             util.print_err(f"error creating the game offer socket {e}")
             game_offer_socket = None
--- util.py
+++ util.py
@@ -1,6 +1,7 @@
 import config
 import time
 
+# see code form https://stackoverflow.com/questions/5574702/how-to-print-to-stderr-in-python
 def print_err(*args, **kwargs):
     print(*args, file=sys.stderr, **kwargs)
 
