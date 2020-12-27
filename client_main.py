35012225c53fe350bb886ff00fa22476f064aaa0
Boaz Rozo <boazrozo@post.bgu.ac.il>
2020-12-27 13:50:47 +0200

    Client game implementation
    First working implementation of the client

--- client_main.py
+++ client_main.py
@@ -12,7 +12,6 @@ from socket_address import SocketAddress
 from client_game_looker import look_for_game
 from client_game_connection import prepare_for_game
 
-fd_stdin = sys.stdin.fileno()
 selector = None
 has_socket_been_registered = False
 has_stdin_been_registered = False
@@ -22,15 +21,13 @@ def main():
     #see the code from https://docs.python.org/2/faq/library.html#how-do-i-get-a-single-keypress-at-a-time
     global fd_stdin
 
-    oldterm = termios.tcgetattr(fd_stdin)
-    newattr = termios.tcgetattr(fd_stdin)
+    oldterm = termios.tcgetattr(sys.stdin)
+    newattr = termios.tcgetattr(sys.stdin)
     newattr[3] = newattr[3] & ~termios.ICANON
-    newattr[6][termios.VMIN] = 0  # cc
-    newattr[6][termios.VTIME] = 0 # cc
     termios.tcsetattr(fd_stdin, termios.TCSANOW, newattr)
 
-    oldflags = fcntl.fcntl(fd_stdin, fcntl.F_GETFL)
-    fcntl.fcntl(fd_stdin, fcntl.F_SETFL, oldflags | os.O_NONBLOCK)
+    oldflags = fcntl.fcntl(sys.stdin, fcntl.F_GETFL)
+    fcntl.fcntl(sys.stdin, fcntl.F_SETFL, oldflags | os.O_NONBLOCK)
 
     try:
         main_logic_loop()
@@ -67,7 +64,6 @@ def main_logic_iter(first_time: bool):
         has_socket_been_registered = False
         has_stdin_been_registered = False
         game_server_addr = look_for_game()
-        #game_server_addr = SocketAddress(('127.0.0.1', 12000))
         game_socket, welcome_msg = prepare_for_game(game_server_addr)
         register_io_for_select(game_socket)
         print(welcome_msg)
@@ -96,22 +92,20 @@ def start_game(game_socket):
 
     while True:
         for (selection_key, events) in selector.select():
-            if selection_key.fileobj is game_socket:
-                print(f"socket ready {events}")
-                if (events & selectors.EVENT_READ) != 0:
-                    game_closed = print_data_from_server(game_socket)
-                    if game_closed:
-                        break
-                if (events & selectors.EVENT_WRITE) != 0:
-                    send_pressed_keys(game_socket)
-                if (events & (selectors.EVENT_WRITE | selectors.EVENT_READ)) == 0:
-                    raise Exception("improper state, events for game socket selection didn't have read nor write")
-            elif selection_key.fileobj is sys.stdin:
-                print("stdin ready")
+            if selection_key.fileobj is sys.stdin:
                 if (events & selectors.EVENT_READ) != 0:
                     buffer_data_from_stdin()
                 else:
                     raise Exception("improper state, events for stdin selection didn't have read")
+            elif selection_key.fileobj is game_socket:
+                if (events & selectors.EVENT_READ) != 0:
+                    game_closed = print_data_from_server(game_socket)
+                    if game_closed:
+                        return
+                if (events & selectors.EVENT_WRITE) != 0:
+                    send_pressed_keys(game_socket)
+                if (events & (selectors.EVENT_WRITE | selectors.EVENT_READ)) == 0:
+                    raise Exception("improper state, events for game socket selection didn't have read nor write")
             else:
                 raise Exception("impossible state, selection was not the game socket nor stdin")
 
